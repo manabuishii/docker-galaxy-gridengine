84b1577326b40cf7d441c04ff1929e4c360c5915
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-09-28 09:35:46 +0000

    Add missing file add line

--- travis_script.sh
+++ travis_script.sh
@@ -22,6 +22,7 @@ docker run -d \
            -v $PWD/outputhostname:/galaxy-central/tools/outputhostname \
            -v $PWD/outputhostname.tool.xml:/galaxy-central/outputhostname.tool.xml \
            -v $PWD/setup_tool.sh:/galaxy-central/setup_tool.sh \
+           -v $PWD/tool_conf.xml:/galaxy-central/tool_conf.xml \
            -v $PWD/act_qmaster:/var/lib/gridengine/default/common/act_qmaster \
            ${GALAXY_CONTAINER} \
            /galaxy-central/setup_tool.sh
