eddb80d03e8387a219d28269efc4916e4bcb028a
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-09-30 00:44:17 +0000

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -3,7 +3,9 @@ docker pull manabuishii/docker-sge-master:0.1.0
 docker pull manabuishii/docker-bioblend:0.8.0
 
 # start master
-docker run --hostname sgemaster --name sgemaster -d -v $PWD/master_script.sh:/usr/local/bin/master_script.sh  manabuishii/docker-sge-master:0.1.0 /usr/local/bin/master_script.sh
+mkdir export
+chmod 777 export
+docker run --hostname sgemaster --name sgemaster -d -v $PWD/export:/export -v $PWD/master_script.sh:/usr/local/bin/master_script.sh  manabuishii/docker-sge-master:0.1.0 /usr/local/bin/master_script.sh
 # wait to sge master
 sleep 10
 
@@ -23,19 +25,27 @@ docker run -d \
            -v $PWD/outputhostname.tool.xml:/galaxy-central/outputhostname.tool.xml \
            -v $PWD/setup_tool.sh:/galaxy-central/setup_tool.sh \
            -v $PWD/tool_conf.xml:/galaxy-central/tool_conf.xml \
+           -v $PWD/galaxy.ini:/galaxy-central/galaxy.ini \                          
            -v $PWD/act_qmaster:/var/lib/gridengine/default/common/act_qmaster \
            ${GALAXY_CONTAINER} \
            /galaxy-central/setup_tool.sh
 echo "Wait 10sec"
 sleep 10
 
-# Add host setting galaxytest to sgemaster
-SGECLIENT=$(docker exec ${GALAXY_CONTAINER_NAME} cat /etc/hosts | grep ${GALAXY_CONTAINER_HOSTNAME})
-docker exec sgemaster bash -c "echo ${SGECLIENT} >> /etc/hosts"
-# Add gridengine client host
-docker exec sgemaster qconf -as ${GALAXY_CONTAINER_HOSTNAME} 
-
+# Add host setting galaxytest to sgemaster                                                                                                                                                              
+echo "Get host info from ${GALAXY_CONTAINER_HOSTNAME}"                                                                                                                                                  
+SGECLIENT=$(docker exec ${GALAXY_CONTAINER_NAME} cat /etc/hosts | grep ${GALAXY_CONTAINER_HOSTNAME})                                                                                                    
+echo "Add host info to sgemaster"                                                                   
+docker exec sgemaster bash -c "echo ${SGECLIENT} >> /etc/hosts ; /etc/init.d/gridengine-master restart"
+echo "Output /etc/hosts on sgemaster"                                                                  
+docker exec sgemaster cat /etc/hosts                                                                   
+echo "Wait 5 sec"                                                                                      
+sleep 5                                                                                                
+# Add gridengine client host                                                                           
+echo "Add submit host ${GALAXY_CONTAINER_HOSTNAME}"                                                    
+docker exec sgemaster bash -c "qconf -as ${GALAXY_CONTAINER_HOSTNAME}"                                 
+echo "Exec test"                                                                                       
 docker run --rm  --link galaxytest:galaxytest -v $PWD/test_outputhostname.py:/work/test_outputhostname.py manabuishii/docker-bioblend:0.8.0 python /work/test_outputhostname.py > out
-grep sgemaster out
-RET=$?
+grep sgemaster out                                                                                                                                                                   
+RET=$?                                                                                                                                                                               
 exit $RET
